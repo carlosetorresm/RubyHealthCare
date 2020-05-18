4cf676964864587b91fd7ef076867009369ea837
Carlos Torres <14030641@itcelaya.edu.mx>
2020-05-18 15:21:14 -0500

    bootstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  #En producción el host deberia cambiar por el host de la aplicación
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   # Do not eager load code on boot.
   config.eager_load = false
 
