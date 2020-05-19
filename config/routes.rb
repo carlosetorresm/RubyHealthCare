edc0dc8184d6dadb4560414ee09ffd0c74cdbde1
Carlos Torres <14030641@itcelaya.edu.mx>
2020-05-19 01:57:16 -0500

    eliminación tablas de pais

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
   get 'registro_medicos/RegistroMedicos'
   post 'registro_medicos/RegistroMedicos'
   get 'registro_medicos/new'
   get 'registro_medicos/create'
-  devise_for :users
->>>>>>> 0597aca5308e2b135a362678845df0744beb2a45
+  
+  devise_for :patients, path: 'patients', controllers: { sessions: "patients/sessions" }
+  devise_for :doctors, path: 'doctors', controllers: { sessions: "doctors/sessions" }
   get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
