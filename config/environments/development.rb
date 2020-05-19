47266c792df042235194b9e13ef9ae614d2c8005
Carlos Torres <14030641@itcelaya.edu.mx>
2020-05-19 01:15:06 -0500

    creacion devise doctor y paciente

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  #Dirección para correos, cambiar host por el host de la plataforma
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -8,9 +11,6 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
-  #Dirección para correos, cambiar host por el host de la plataforma
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Show full error reports.
   config.consider_all_requests_local = true
